37cf9a5c5877675c69295aa2e32f2597a5d25d79
Felipe Ferreira <feferreira.sf@gmail.com>
2024-07-06 14:28:26 -0300

    💄 change loading icon

--- lib/app/core/ui/widgets/sd_sb_loader.dart
+++ lib/app/core/ui/widgets/sd_sb_loader.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:saldo_sabio/app/core/ui/theme/sd_sb_colors.dart';
 
 class SdSbLoader extends StatelessWidget {
   const SdSbLoader({super.key});
@@ -7,8 +8,12 @@ class SdSbLoader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: LoadingAnimationWidget.prograssiveDots(
-        color: Colors.amber,
+      // child: LoadingAnimationWidget.prograssiveDots(
+      //   color: SdSbThemeColors.greenLight,
+      //   size: 50,
+      // ),
+      child: LoadingAnimationWidget.dotsTriangle(
+        color: SdSbThemeColors.white,
         size: 50,
       ),
     );
